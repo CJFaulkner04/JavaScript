5811ee1743a2a1538d0221be48a3bff21183009d
Carlos Faulkner <drcjfaulkner@gmail.com>
2023-04-05 15:10:20 -0400

    emails

--- Email.js
+++ Email.js
@@ -17,7 +17,7 @@ let appData = {
      emails: [
         {type: 'starred', from: 'Shady guy', subject: 'Shady stuff', message: 'Get the job done at 12 am on the dot. No mishaps, we cannot afford to go back to prison.' },
         {type: 'snoozed', from: 'Mom', subject: 'Suprise in Lunch', message: 'Hey I packed a fruit roll up in your lunch' },
-        {type: 'drafts', sent: false, from: 'Me', to: 'Shady Guy', subject: 'RE:Shady stuff', message: 'I WANT MY END OF THE DEAL' },
+        {type: 'drafts', from: 'Me', to: 'Shady Guy', subject: 'RE:Shady stuff', message: 'I WANT MY END OF THE DEAL' },
         {type: 'sent', from: 'Me', to: 'Shady Guy', subject: 'RE:Shady stuff', message: "Got the job done, don't forget that we are in this 50/50" },
         {type: 'junk', from: 'IRS', to: 'Me', subject: 'We are comming', message: "You haven't paid taxes in years, we WILL find you." }
     ] 
@@ -40,7 +40,7 @@ for(let i = 0; i < appData.emails.length; i++){
 console.log('from: ' + appData.emails[1].from, '     Subject: ' + appData.emails[1].subject);
 console.log('message: '+ appData.emails[1].message);
 
-appData.emails[2].sent = true;
+appData.emails[2].type = 'sent';
 
 let draft = {type: 'draft', from: 'Me', to: 'IRS', subject: 'My bad', message: "Im sorry, but i'm in too deep, and owe too much money, I can pay it back in full once Shady Guy gives me my money." }
 
